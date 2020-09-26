046c0e97bea2d31ea3c376c9e77556fd002df943
Ingo Griebsch <ingo.griebsch@gmx.de>
2020-09-26 20:34:03 +0200

    feat(#94): allow to add SirenActionField type-mappings per configuration

--- src/main/java/de/ingogriebsch/spring/hateoas/siren/SirenConfiguration.java
+++ src/main/java/de/ingogriebsch/spring/hateoas/siren/SirenConfiguration.java
@@ -15,7 +15,15 @@
  */
 package de.ingogriebsch.spring.hateoas.siren;
 
-import lombok.Value;
+import static com.google.common.collect.Lists.newArrayList;
+import static de.ingogriebsch.spring.hateoas.siren.TypeBasedSirenActionFieldTypeConverter.TypeMapping.mapping;
+import static lombok.AccessLevel.PACKAGE;
+
+import java.util.List;
+
+import de.ingogriebsch.spring.hateoas.siren.TypeBasedSirenActionFieldTypeConverter.TypeMapping;
+import lombok.Getter;
+import lombok.NonNull;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 
@@ -24,18 +32,11 @@ import org.springframework.hateoas.EntityModel;
  *
  * @author Ingo Griebsch
  */
-@Value
+@Getter(PACKAGE)
 public class SirenConfiguration {
 
-    boolean entityAndCollectionModelSubclassingEnabled;
-
-    public SirenConfiguration() {
-        this(false);
-    }
-
-    private SirenConfiguration(boolean entityAndCollectionModelSubclassingEnabled) {
-        this.entityAndCollectionModelSubclassingEnabled = entityAndCollectionModelSubclassingEnabled;
-    }
+    private boolean entityAndCollectionModelSubclassingEnabled = false;
+    private List<TypeMapping> actionFieldTypeMappings = newArrayList();
 
     /**
      * Configures whether it is possible to subclass {@link EntityModel}s and {@link CollectionModel}s.
@@ -45,7 +46,23 @@ public class SirenConfiguration {
      * @return the updated configuration reflecting the setting
      */
     public SirenConfiguration withEntityAndCollectionModelSubclassingEnabled(boolean enabled) {
-        return new SirenConfiguration(enabled);
+        entityAndCollectionModelSubclassingEnabled = enabled;
+        return this;
     }
 
+    /**
+     * Configures additional mappings that are used to convert the type of a payload field into a Siren action field type.
+     * <p>
+     * These mappings override the default mappings that are defined inside the library. The order of the defined mappings is
+     * important because the class hierarchy is taken into consideration.
+     * 
+     * @param source the type of the member that is part of the payload that is transformed into Siren action fields.
+     * @param target the {@link SirenActionFieldType type} of the Siren Action field that should represent the {@literal source}
+     *        type in the Siren representation.
+     * @return the updated configuration reflecting the setting
+     */
+    public SirenConfiguration withActionFieldTypeMappging(@NonNull Class<?> source, @NonNull SirenActionFieldType target) {
+        actionFieldTypeMappings.add(mapping(source, target));
+        return this;
+    }
 }
---
+++ src/test/java/de/ingogriebsch/spring/hateoas/siren/SirenConfigurationTest.java
@@ -0,0 +1,68 @@
+package de.ingogriebsch.spring.hateoas.siren;
+
+import static de.ingogriebsch.spring.hateoas.siren.SirenActionFieldType.NUMBER;
+import static de.ingogriebsch.spring.hateoas.siren.SirenActionFieldType.TEXT;
+import static de.ingogriebsch.spring.hateoas.siren.TypeBasedSirenActionFieldTypeConverter.TypeMapping.mapping;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SirenConfigurationTest {
+
+    @Test
+    void ctor_should_define_default_for_entityAndCollectionModelSubclassingEnabled() {
+        assertThat(new SirenConfiguration().isEntityAndCollectionModelSubclassingEnabled()).isFalse();
+    }
+
+    @Test
+    void ctor_should_define_default_for_actionFieldTypeMappings() {
+        assertThat(new SirenConfiguration().getActionFieldTypeMappings()).isEmpty();
+    }
+
+    @Test
+    void withEntityAndCollectionModelSubclassingEnabled_should_return_set_value() {
+        SirenConfiguration configuration = new SirenConfiguration();
+        assertThat(configuration.isEntityAndCollectionModelSubclassingEnabled()).isFalse();
+
+        configuration.withEntityAndCollectionModelSubclassingEnabled(true);
+        assertThat(configuration.isEntityAndCollectionModelSubclassingEnabled()).isTrue();
+    }
+
+    @Test
+    void configuration_should_be_kept_if_wither_are_used() {
+        SirenConfiguration configuration = new SirenConfiguration();
+        assertThat(configuration.isEntityAndCollectionModelSubclassingEnabled()).isFalse();
+
+        configuration.withEntityAndCollectionModelSubclassingEnabled(true);
+        assertThat(configuration.isEntityAndCollectionModelSubclassingEnabled()).isTrue();
+
+        configuration.withActionFieldTypeMappging(String.class, TEXT);
+        assertThat(configuration.getActionFieldTypeMappings()).containsExactly( //
+            mapping(String.class, TEXT) //
+        );
+
+        assertThat(configuration.isEntityAndCollectionModelSubclassingEnabled()).isTrue();
+
+        configuration.withEntityAndCollectionModelSubclassingEnabled(false);
+        assertThat(configuration.isEntityAndCollectionModelSubclassingEnabled()).isFalse();
+
+        configuration.withActionFieldTypeMappging(Integer.class, NUMBER);
+        assertThat(configuration.getActionFieldTypeMappings()).containsExactly( //
+            mapping(String.class, TEXT), //
+            mapping(Integer.class, NUMBER) //
+        );
+    }
+
+    @Test
+    void withActionFieldTypeMappging_should_return_set_value() {
+        SirenConfiguration configuration = new SirenConfiguration();
+        assertThat(configuration.getActionFieldTypeMappings()).isEmpty();
+
+        configuration.withActionFieldTypeMappging(String.class, TEXT);
+        configuration.withActionFieldTypeMappging(Integer.class, NUMBER);
+        assertThat(configuration.getActionFieldTypeMappings()).containsExactly( //
+            mapping(String.class, TEXT), //
+            mapping(Integer.class, NUMBER) //
+        );
+    }
+}
